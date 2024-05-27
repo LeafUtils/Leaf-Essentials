f6271d7029b2556b51b5b013e627fb1653bc775e
TheLegendaryTrashcan <122332042+Ant767@users.noreply.github.com>
2024-05-27 17:41:56 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,21 @@
-import { world } from '@minecraft/server';
-world.sendMessage("Hello, world!")
+import { system, ScriptEventSource } from '@minecraft/server';
+import uiManager from './uiManager';
+import config from './config';
+import './uis/uiBuilder/root';
+system.afterEvents.scriptEventReceive.subscribe(e=>{
+    if(
+        e.id == config.scripteventNames.openDefault &&
+        e.sourceType == ScriptEventSource.Entity &&
+        e.sourceEntity.typeId == "minecraft:player"
+    ) {
+        uiManager.open(e.sourceEntity, e.message)
+    }
+})
+// let id = uiBuilder.createUI("test", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "normal", "test");
+// uiBuilder.addButtonToUI(
+//     id,
+//     "Test",
+//     "Working on UI maker",
+//     "/say hi",
+//     "vanilla/iron_sword"
+// )
