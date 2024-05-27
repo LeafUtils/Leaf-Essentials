4c992a03b97e7f437dca8a57aad77b7e79ff3096
TheLegendaryTrashcan <122332042+Ant767@users.noreply.github.com>
2024-05-27 17:41:33 -0500

    Added icon pack: vanilla

---
+++ scripts/icon_packs/vanilla.js
@@ -0,0 +1,8 @@
+let map = new Map([
+    ["pack_name", "Vanilla"],
+    ["pack_icon", "textures/items/iron_sword"],
+    ["pack_namespace", "vanilla"],
+    ["pack_data", new Map([
+        ["iron_sword", "textures/items/iron_sword"]
+    ])]
+])
