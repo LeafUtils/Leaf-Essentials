71bfb656e737a46afded4857d1e01111a065ff02
Ant767 <antspa767@gmail.com>
2024-07-26 20:42:47 -0500

    port !what from azalea

---
+++ scripts/commands/what.js
@@ -0,0 +1,44 @@
+import commandManager from "../api/commands/commandManager";
+import { prismarineDb } from "../lib/prismarinedb";
+let db = prismarineDb.keyval("WhatList");
+let theme = {
+    category: "§8",
+    description: "§7",
+    command: "§a"
+}
+commandManager.addCommand("what", {
+    description: "Does shit",
+    category: "Fun",
+    author: "ZSStudios + TRASH",
+    usage: "!what <post | view> [text]"
+}, ({msg, args})=>{
+    if(!db.get("WhatList")) db.set("WhatList", "[]");
+    let whatList = JSON.parse(db.get("WhatList") ? db.get("WhatList") : "[]");
+    if(args.length) { // checks if theres an argument
+        if(args[0] == "post") {
+            // list.push(args.slice(1).join(' '));
+            whatList.push({
+                t: args.slice(1).join(' '),
+                s: msg.sender.name
+            });
+            db.set("WhatList", JSON.stringify(whatList));
+            msg.sender.success(`Posted!`)
+        } else if(args[0] == "view") {
+            let text = [];
+            text.push(`${theme.category}<-=- ${theme.command}What List ${theme.category}-=->`);
+            for(const post of whatList) {
+                text.push(`${theme.command}${post.s} ${theme.description}${post.t}§r`);
+            }
+            text.push(``);
+            response(`TEXT ${text.join('\n')}`);
+        }
+    } else {
+        let text = [];
+        text.push(`${theme.category}<-=- ${theme.command}What List ${theme.category}-=->`);
+        for(const post of whatList) {
+            text.push(`§a${post.s} §7${theme.description}${post.t}§r`);
+        }
+        text.push(``);
+        msg.sender.sendMessage(text.join('\n'));
+    }
+})
